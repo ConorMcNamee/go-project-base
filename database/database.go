65f447c4012cbbc9e006c2c96c3e85b4398c26be
40298979 <cmcnamee25@qub.ac.uk>
2024-03-14 14:26:00 +0000

    Created register route

--- database/database.go
+++ database/database.go
@@ -28,4 +28,16 @@ func GetUserByID(id int64) (models.User, error) {
 	return u, nil
 }
 
-func CreateNewUser() {}
+func CreateNewUser(u models.User) (int64, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `insert into users (first_name, last_name, email, password, age, admin, joined_at, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+
+	_, err := app.DB.SQL.ExecContext(ctx, query, u.FirstName, u.LastName, u.Email, u.Password, u.Age, u.Admin, u.Joined_at, u.Created_at, u.Updated_at)
+
+	if err != nil {
+		panic(err)
+	}
+	return 0, nil
+}
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,16 +3,12 @@ package handlers
 import (
 	"fmt"
 	"gobaseproject/database"
+	"gobaseproject/models"
 	"net/http"
+	"time"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	user, err := database.GetUserByID(1)
-	if err != nil {
-		fmt.Println("Cannot find user with that ID")
-	}
-
-	fmt.Fprintf(w, "Hey: %s", user.FirstName)
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -24,5 +20,40 @@ func Logout(w http.ResponseWriter, r *http.Request) {
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
+	// Restrict this route to POST only
 
+	r.ParseForm()
+	// Check to see if everything is of valid length's and types
+
+	// Hash Password
+
+	// Store in Database
+	var u models.User
+	u.FirstName = r.FormValue("first_name")
+	u.LastName = r.FormValue("first_name")
+	u.Email = r.FormValue("email")
+	u.Password = r.FormValue("password") // Stores password in plain text. Must be changed. THIS IS PURELY FOR TESTING PURPOSES
+	u.Admin = false
+	u.Age = 18
+	u.Joined_at = time.Now()
+	u.Created_at = time.Now()
+	u.Updated_at = time.Now()
+
+	for key, value := range r.Form {
+		fmt.Printf("%s = %s\n", key, value)
+	}
+
+	fmt.Println(u, r.FormValue("first_name"))
+	fmt.Println("Creating User!")
+	result, err := database.CreateNewUser(u)
+
+	if err != nil {
+		fmt.Println("Creating user failed")
+	}
+
+	if result == 0 {
+		fmt.Println("Creating User was Successful!")
+	}
+
+	// Add JSON response
 }
